3c7d91c9ca18c433255377d317944063db2a23ef
handev17 <handev17@gmail.com>
2022-03-27 22:34:28 -0400

    Second commit

--- src/main/java/com/fundamentosplatzi/springboot/fundamentos/FundamentosApplication.java
+++ src/main/java/com/fundamentosplatzi/springboot/fundamentos/FundamentosApplication.java
@@ -1,10 +1,16 @@
 package com.fundamentosplatzi.springboot.fundamentos;
 
+import com.fundamentosplatzi.springboot.fundamentos.component.ComponentDependency;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class FundamentosApplication {
+	private ComponentDependency componentDependency;
+	FundamentosApplication(ComponentDependency componentDependency){
+		this.componentDependency = componentDependency;
+
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(FundamentosApplication.class, args);
