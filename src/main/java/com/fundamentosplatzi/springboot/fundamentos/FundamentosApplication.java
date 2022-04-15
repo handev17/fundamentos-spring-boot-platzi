0ad7951f82f401b320be6a72922f6c23a15ada6a
handev17 <handev17@gmail.com>
2022-04-15 19:28:08 -0400

    Registro en base de datos con JpaRepository

--- src/main/java/com/fundamentosplatzi/springboot/fundamentos/FundamentosApplication.java
+++ src/main/java/com/fundamentosplatzi/springboot/fundamentos/FundamentosApplication.java
@@ -4,7 +4,10 @@ import com.fundamentosplatzi.springboot.fundamentos.bean.MyBean;
 import com.fundamentosplatzi.springboot.fundamentos.bean.MyBeanWithDependency;
 import com.fundamentosplatzi.springboot.fundamentos.bean.MyBeanWithProperties;
 import com.fundamentosplatzi.springboot.fundamentos.component.ComponentDependency;
+import com.fundamentosplatzi.springboot.fundamentos.entity.User;
 import com.fundamentosplatzi.springboot.fundamentos.pojo.UserPojo;
+import com.fundamentosplatzi.springboot.fundamentos.repository.PostRepository;
+import com.fundamentosplatzi.springboot.fundamentos.repository.UserRepository;
 import org.apache.juli.logging.Log;
 import org.apache.juli.logging.LogFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,6 +15,10 @@ import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.time.LocalDate;
+import java.util.Arrays;
+import java.util.List;
 
 @SpringBootApplication
 public class FundamentosApplication implements CommandLineRunner {
@@ -21,16 +28,20 @@ public class FundamentosApplication implements CommandLineRunner {
 	private MyBeanWithDependency myBeanWithDependency;
 	private MyBeanWithProperties  myBeanWithProperties;
 	private UserPojo userPojo;
+	private UserRepository userRepository;
+	private PostRepository postRepository;
 
 
 
 	@Autowired
-	public FundamentosApplication(@Qualifier("componentTwoImplement") ComponentDependency componentDependency, MyBean myBean, MyBeanWithDependency myBeanWithDependency, MyBeanWithProperties  myBeanWithProperties, UserPojo userPojo){
+	public FundamentosApplication(@Qualifier("componentTwoImplement") ComponentDependency componentDependency, MyBean myBean, MyBeanWithDependency myBeanWithDependency, MyBeanWithProperties  myBeanWithProperties, UserPojo userPojo, UserRepository userRepository, PostRepository postRepository){
 		this.componentDependency = componentDependency;
 		this.myBean = myBean;
 		this.myBeanWithDependency = myBeanWithDependency;
 		this.myBeanWithProperties = myBeanWithProperties;
 		this.userPojo = userPojo;
+		this.userRepository = userRepository;
+		this.postRepository = postRepository;
 
 
 	}
@@ -41,6 +52,24 @@ public class FundamentosApplication implements CommandLineRunner {
 
 	@Override
 	public void run(String... args)  {
+		//ejemplosAnteriores();
+		saveUsersIndDatabase();
+
+
+	}
+
+	private void saveUsersIndDatabase(){
+		User user1 = new User("Jonh","jonh@gmail.com", LocalDate.of(2021,03,20));
+		User user2 = new User("Jonh1","jonh1@gmail.com", LocalDate.of(2021,04,21));
+		User user3 = new User("Jonh2","jonh2@gmail.com", LocalDate.of(2021,05,22));
+		User user4 = new User("Jonh3","jonh3@gmail.com", LocalDate.of(2021,06,23));
+		User user5 = new User("Jonh4","jonh4@gmail.com", LocalDate.of(2021,07,24));
+		User user6 = new User("Jonh5","jonh5@gmail.com", LocalDate.of(2021,03,25));
+		List <User> list = Arrays.asList(user1,user2,user3,user4,user5);
+		list.stream().forEach(userRepository :: save);
+	}
+
+	private void ejemplosAnteriores(){
 		componentDependency.saludar();
 		myBean.print();
 		myBeanWithDependency.printWithDependency();
@@ -53,6 +82,5 @@ public class FundamentosApplication implements CommandLineRunner {
 		}catch(Exception e){
 			LOGGER.error("Esto es un error al dividir por cero "+e.getMessage());
 		}
-
 	}
 }
